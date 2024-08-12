60c39c0d5d943e10a5e664f52b308acced30afa2
abcmehedi5 <abcmehedi5@gmail.com>
2024-08-13 00:36:01 +0600

    Check loggedin user

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,19 +1,48 @@
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import HomeScreen from '../Screens/Home/HomeScreen';
 import ListScreen from '../Screens/ListScreen/ListScreen';
 import CategoryScreen from '../Screens/CategoryScreen/CategoryScreen';
 import VocabularyScreen from '../Screens/VocabularyScreen/VocabularyScreen';
 import MessageScreen from '../Screens/MessageScreen/MessageScreen';
 import LoginScreen from '../Screens/Auth/Login/LoginScreen';
+import {ActivityIndicator, View} from 'react-native';
+
 const Navigation = () => {
   const Stack = createNativeStackNavigator();
-  const isUser = false;
-  const initialPath = isUser ? 'Home' : 'login';
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const token = await AsyncStorage.getItem('accessToken');
+        if (token) {
+          setIsUserLoggedIn(true);
+        }
+      } catch (error) {
+        console.error('Failed to fetch access token:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={initialPath}>
+      <Stack.Navigator initialRouteName={isUserLoggedIn ? 'Home' : 'login'}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
